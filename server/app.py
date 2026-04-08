02c1ef9b86b3aa120fa8d0e52897438b3d5d006e
ravizf <234827561+ravizf@users.noreply.github.com>
2026-04-09 02:46:59 +0530

    Define callable server main wrapper

--- server/app.py
+++ server/app.py
@@ -1,3 +1,12 @@
-from app import app, main
+from app import app as fastapi_app
+from app import main as root_main
+
+
+app = fastapi_app
+
+
+def main():
+    return root_main()
+
 
 __all__ = ["app", "main"]
